607eb607c0e0669d2f62bcd8634d9d1b765eea54
Leona Varghese <leonavarghesewh@gmail.com>
2023-09-22 12:19:23 +0530

    Testimonial & Brands

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,6 @@ const Button = ({ styles }) => {
   return (
     <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none rounded-lg ${styles}`}>
     Get Started
-
     </button>
    
   )
--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,11 +1,21 @@
+import { card } from '../assets';
+import styles, {layout} from '../style';
+import Button from './Button';
 
+const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>Find a better card deal <br className="sm:block hidden" /> in a few easy steps.</h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>New Masterclass Cohort starts October 23rd! Land your dream job in a project-based bootcamp with expert mentor feedback, code reviews, and career prep.
+      </p>
+      <Button styles="mt-10" />
+    </div>
 
-const CardDeal = () => {
-  return (
-    <div>
+    <div className={layout.sectionImg}>
+      <img src={card} alt="card" className='w-[100%] h-[100%]' />
       
     </div>
-  )
-}
+  </section>
+)
 
 export default CardDeal;
