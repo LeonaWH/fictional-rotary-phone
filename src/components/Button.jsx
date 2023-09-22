a266e422ca387660b3dbbdab87b728ec016686c1
Leona Varghese <leonavarghesewh@gmail.com>
2023-09-22 10:37:21 +0530

    Features Section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,11 @@
-
-const Button = () => {
+import styles from "../style";
+const Button = ({ styles }) => {
   return (
-    <div>
-      
-    </div>
+    <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none rounded-lg ${styles}`}>
+    Get Started
+
+    </button>
+   
   )
 }
 
